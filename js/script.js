d1d2575e32b940ee9e514dbc71cf5e602fd779c6
Alisson Batalha <82676146+alissonbatalha@users.noreply.github.com>
2023-04-24 17:28:56 -0300

    Add files via upload
    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const btnCalcula = document.querySelector('#calcular');
+
+btnCalcula.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let valorImovel = document.querySelector('input#valor-imovel');
+  let valorImovelValue = Number(valorImovel.value);
+
+  const rendaMensal = document.querySelector('input#renda-mensal');
+  const rendaMensalValue = Number(rendaMensal.value);
+
+  const duracao = document.querySelector('input#duracao');
+  const duracaoValue = Number(duracao.value);
+
+  const labelEntrada = document.querySelector('.label-entrada');
+  const entrada = document.querySelector('input#entrada');
+  const entradaValue = Number(entrada.value);
+  const semEntrada = document.querySelector('input#sem-entrada');
+
+  const resultado = document.querySelector(".resultado");
+  const resumoValor = document.querySelector('#resumo-valor');
+  const resumoRenda = document.querySelector('#resumo-renda');
+  const resumoDuracao = document.querySelector('#resumo-duracao');
+  const resumoEntrada = document.querySelector('#resumo-entrada');
+  const resumoFinanciamento = document.querySelector('#resumo-financiamento');
+  const resumoDecisao = document.querySelector('#resumo-decisao');
+
+  semEntrada.addEventListener('click', (event) => {
+    event.preventDefault;
+
+    labelEntrada.classList.toggle('hidden');
+    entrada.classList.toggle('hidden');
+    resumoEntrada.classList.toggle('hidden');
+    
+  });
+
+
+  if (entradaValue > 0) {
+    valorImovelValue -= entradaValue;
+  }
+
+  let tercoDoSalario = (rendaMensalValue / 100) * 30;
+
+  const quantidadeParcelas = duracaoValue * 12;
+
+  let valorParcelas = valorImovelValue / quantidadeParcelas;
+
+  let jurosMensais = (valorParcelas / 100) * 50;
+
+  let parcelaComTaxa = valorParcelas + jurosMensais;
+
+  if (parcelaComTaxa >= tercoDoSalario) {
+    resumoDecisao.classList.add('reprovado');
+    resumoDecisao.innerHTML += 'REPROVADO';
+  } else {
+    resumoDecisao.classList.add('provado');
+    resumoDecisao.innerHTML += 'APROVADO';
+  }
+
+  resultado.classList.add('ativo');
+
+  console.table(`${valorImovel.value}, ${quantidadeParcelas}, ${valorParcelas} ,${parcelaComTaxa}, ${tercoDoSalario}, ${parcelaComTaxa * duracaoValue}`);
+
+  resumoValor.innerHTML += ` R$ ${valorImovelValue + entradaValue},00`;
+  resumoRenda.innerHTML += ` R$ ${rendaMensalValue},00`;
+  resumoDuracao.innerHTML += ` ${quantidadeParcelas} (${duracaoValue} anos)`;
+  resumoEntrada.innerHTML += ` R$ ${entradaValue},00`;
+  resumoFinanciamento.innerHTML += ` ${duracaoValue} parcelas de R$ ${parcelaComTaxa},00`;
+ //resumoDecisao.innerHTML += ` `;
+});
